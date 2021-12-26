aae1591121c23d06f36986d4c2bf22d4388b4d00
EillesWan <W-YI_DoctorYI@outlook.com>
2021-12-26 18:02:41 +0800

    探索BeeWare的功能ing，并无实质性的更新。

--- src/oddevenmatrix/app.py
+++ src/oddevenmatrix/app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 一个简单的数字棋盘游戏
 """
@@ -16,11 +17,35 @@ class OddEvenMatrix(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        noticeLabel = toga.Label(
+            '执行 >>>',
+            style=Pack(padding=(0, 5))
+        )
+        self.inputBox = toga.TextInput(style=Pack(flex=1))
+
+        cmd_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        cmd_box.add(noticeLabel)
+        cmd_box.add(self.inputBox)
+
+        button = toga.Button(
+            'Run',
+            on_press=self.runrun,
+            style=Pack(padding=5)
+        )
+
+        
+
+        main_box.add(cmd_box)
+        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def runrun(self, widget):
+        print("Run ", self.inputBox.value)
 
 
 def main():
